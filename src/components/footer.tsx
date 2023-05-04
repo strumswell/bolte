8caba5bc2b3605537bf2a23e187c1d91290755be
Philipp Bolte <philipp.bukkit@gmail.com>
2023-05-04 23:40:18 +0200

    chore: favicon and config

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
     <nav className="flex justify-between w-auto bg-zinc-900 text-xl font-semibold p-4 pl-7 pr-8 mt-4 mb-28 sm:mb-4 ml-4 mr-4 sm:ml-0 sm:mr-0 rounded-full border border-zinc-800">
       <p className="text-zinc-200">philipp bolte.</p>
       <p className="text-zinc-500">
-        <Link href="/privacy">privacy</Link> • all rights reserved. © {new Date().getUTCFullYear()}
+        <Link href="/privacy">privacy</Link> • © {new Date().getUTCFullYear()}
       </p>
     </nav>
   );
