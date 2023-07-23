8781d0abd8f80f4fe253e0311652c3754a08d5bf
Philipp Bolte <philipp.bolte@spherity.com>
2023-07-23 17:50:34 +0200

    feat: play around with weather animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,20 @@ module.exports = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      keyframes: {
+        moveCloud: {
+          "0%": { transform: "translateX(-100%)" },
+          "100%": { transform: "translateX(100%)" },
+        }
+      },
+      animation: {
+        moveCloud30s: "moveCloud 30s linear infinite",
+        moveCloud35s: "moveCloud 35s linear infinite",
+        moveCloud40s: "moveCloud 40s linear infinite",
+        moveCloud45s: "moveCloud 45s linear infinite",
+        moveCloud60s: "moveCloud 60s linear infinite",
+        moveCloud90s: "moveCloud 90s linear infinite",
+      }
     },
   },
   plugins: [],
